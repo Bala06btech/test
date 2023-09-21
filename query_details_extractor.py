504358b8829a45da4203289237281b920674f49e
Bala06btech <bala06btech@gmail.com>
2023-09-21 09:56:43 -0400

    Update query_details_extractor.py

--- query_details_extractor.py
+++ query_details_extractor.py
@@ -26,25 +26,57 @@ def process_s3_py_file(bucket_name, file_key):
     response = s3.get_object(Bucket=bucket_name, Key=file_key)
     file_content = response['Body'].read().decode('utf-8')
     
-    # Use regular expressions to find Spark SQL queries
-    sql_queries = re.findall(r'spark\.sql\(.*?\.sql\(["\'](.*?)["\']\)', file_content, re.DOTALL)
+    # Split the file content into lines
+    lines = file_content.split('\n')
     
-    # Extract table names and columns from each SQL query
-    for idx, query in enumerate(sql_queries, start=1):
-        table_names, columns_used = extract_table_and_columns(query)
+    # Initialize line number
+    line_number = 0
+    
+    # Initialize query buffer
+    query_buffer = []
+    
+    # Iterate through the lines of the file
+    for line in lines:
+        # Increment the line number
+        line_number += 1
         
-        data.append({
-            "Query Number": idx,
-            "Table Names": ', '.join(table_names),
-            "Columns Used": ', '.join(columns_used)
-        })
+        # Check if the line contains a Spark SQL query
+        if 'spark.sql' in line:
+            # Start building the query
+            query_buffer.append(line)
+            
+            # Keep reading lines until the query is complete
+            while not line.strip().endswith(')'):
+                line = lines[line_number]
+                query_buffer.append(line)
+                line_number += 1
+            
+            # Join the lines to form the complete query
+            query = '\n'.join(query_buffer)
+            
+            # Use regular expressions to find Spark SQL queries
+            sql_queries = re.findall(r'spark\.sql\(.*?\.sql\(["\'](.*?)["\']\)', query, re.DOTALL)
+            
+            # Extract table names and columns from each SQL query
+            for idx, query in enumerate(sql_queries, start=1):
+                table_names, columns_used = extract_table_and_columns(query)
+                
+                data.append({
+                    "File": file_key,
+                    "Line Number": line_number,
+                    "Query Number": idx,
+                    "Table Names": ', '.join(table_names),
+                    "Columns Used": ', '.join(columns_used)
+                })
 
 # AWS S3 bucket details
 bucket_name = 'your-s3-bucket-name'
 prefix = 'path/to/your/py/files'
 
-# List objects in the S3 bucket
+# Create an S3 client
 s3 = boto3.client('s3')
+
+# List objects in the S3 bucket
 objects = s3.list_objects_v2(Bucket=bucket_name, Prefix=prefix)
 
 # Iterate through S3 objects and process each .py file
