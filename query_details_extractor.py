fe0646824ebdc9f9d3e95cd3153862ffae92d634
Bala06btech <bala06btech@gmail.com>
2023-09-21 09:50:42 -0400

    Update query_details_extractor.py

--- query_details_extractor.py
+++ query_details_extractor.py
@@ -1,6 +1,6 @@
 import re
-import os
 import pandas as pd
+import boto3
 
 # Define a function to extract table names and columns used from a Spark SQL query
 def extract_table_and_columns(query):
@@ -17,36 +17,45 @@ def extract_table_and_columns(query):
 # Define a list to store the extracted data
 data = []
 
-# Define a function to process a .py file and extract table and column details
-def process_py_file(file_path):
-    with open(file_path, 'r') as file:
-        content = file.read()
+# Define a function to process a .py file from S3 and extract table and column details
+def process_s3_py_file(bucket_name, file_key):
+    # Create an S3 client
+    s3 = boto3.client('s3')
+    
+    # Download the .py file from S3
+    response = s3.get_object(Bucket=bucket_name, Key=file_key)
+    file_content = response['Body'].read().decode('utf-8')
+    
+    # Use regular expressions to find Spark SQL queries
+    sql_queries = re.findall(r'spark\.sql\(.*?\.sql\(["\'](.*?)["\']\)', file_content, re.DOTALL)
+    
+    # Extract table names and columns from each SQL query
+    for idx, query in enumerate(sql_queries, start=1):
+        table_names, columns_used = extract_table_and_columns(query)
         
-        # Use regular expressions to find Spark SQL queries
-        sql_queries = re.findall(r'spark\.sql\(.*?\.sql\(["\'](.*?)["\']\)', content, re.DOTALL)
-        
-        # Extract table names and columns from each SQL query
-        for idx, query in enumerate(sql_queries, start=1):
-            table_names, columns_used = extract_table_and_columns(query)
-            
-            data.append({
-                "File": file_path,
-                "Query Number": idx,
-                "Table Names": ', '.join(table_names),
-                "Columns Used": ', '.join(columns_used)
-            })
+        data.append({
+            "Query Number": idx,
+            "Table Names": ', '.join(table_names),
+            "Columns Used": ', '.join(columns_used)
+        })
 
-# Define the directory containing the .py files
-directory = '/path/to/your/py/files'
+# AWS S3 bucket details
+bucket_name = 'your-s3-bucket-name'
+prefix = 'path/to/your/py/files'
 
-# Iterate through .py files in the directory and process each file
-for filename in os.listdir(directory):
-    if filename.endswith('.py'):
-        file_path = os.path.join(directory, filename)
-        process_py_file(file_path)
+# List objects in the S3 bucket
+s3 = boto3.client('s3')
+objects = s3.list_objects_v2(Bucket=bucket_name, Prefix=prefix)
+
+# Iterate through S3 objects and process each .py file
+for obj in objects.get('Contents', []):
+    file_key = obj['Key']
+    
+    # Process the .py file from S3
+    process_s3_py_file(bucket_name, file_key)
 
 # Create a DataFrame from the extracted data
-df = pd.DataFrame(data)
+query_details_dataframe = pd.DataFrame(data)
 
 # Display the DataFrame
-print(df)
+print(query_details_dataframe)
