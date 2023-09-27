12fad6e48393c7b0cdb97fcb18d5a11a70f5b043
Bala06btech <bala06btech@gmail.com>
2023-09-27 15:33:03 -0400

    Update delete_folders_with_suffix_recursive.py

--- delete_folders_with_suffix_recursive.py
+++ delete_folders_with_suffix_recursive.py
@@ -21,3 +21,48 @@ bucket_name = 'your-bucket-name'
 root_folder = 'your-root-folder/'
 
 delete_folders_with_suffix_recursive(bucket_name, root_folder)
+
+#########################################
+
+import boto3
+import urllib.parse
+
+def parse_s3_uri(s3_uri):
+    parsed_uri = urllib.parse.urlparse(s3_uri)
+    if parsed_uri.scheme != 's3':
+        raise ValueError("Not an S3 URI")
+    
+    bucket_name = parsed_uri.netloc
+    root_folder = parsed_uri.path.lstrip('/')
+    return bucket_name, root_folder
+
+def list_folders(bucket_name, root_folder):
+    s3 = boto3.client('s3')
+    folders = []
+    
+    def list_folders_recursive(prefix):
+        objects = s3.list_objects_v2(Bucket=bucket_name, Prefix=prefix, Delimiter='/')
+        
+        for obj in objects.get('CommonPrefixes', []):
+            folder_name = obj['Prefix']
+            folders.append(folder_name)
+            list_folders_recursive(folder_name)
+    
+    list_folders_recursive(root_folder)
+    return folders
+
+def delete_folders_with_suffix(bucket_name, folders_to_delete):
+    s3 = boto3.client('s3')
+    
+    for folder in folders_to_delete:
+        if folder.endswith('$folder$/'):
+            print(f'Deleting folder: {folder}')
+            s3.delete_object(Bucket=bucket_name, Key=folder)
+
+# Replace this with your S3 URI
+s3_uri = 's3://your-bucket-name/your-root-folder/'
+
+bucket_name, root_folder = parse_s3_uri(s3_uri)
+folders = list_folders(bucket_name, root_folder)
+delete_folders_with_suffix(bucket_name, folders)
+
