5728a910501db1e226537c8f7e6c6fc388a10d59
Bala06btech <bala06btech@gmail.com>
2024-03-04 10:43:28 -0500

    Update s3_older_cleanup.py

--- s3_older_cleanup.py
+++ s3_older_cleanup.py
@@ -32,5 +32,37 @@ def list_old_files(bucket, folder):
     else:
         print(f"No files older than 30 days found in '{folder}'.")
 
+def delete_old_files(bucket, folder):
+    # Calculate the date 30 days ago from now
+    thirty_days_ago = datetime.now(timezone.utc) - timedelta(days=30)
+    
+    # List all objects within the specified folder
+    objects_to_delete = []
+    paginator = s3.get_paginator('list_objects_v2')
+    page_iterator = paginator.paginate(Bucket=bucket, Prefix=folder)
+    
+    for page in page_iterator:
+        if 'Contents' in page:
+            for obj in page['Contents']:
+                # If the object is older than 30 days, mark it for deletion
+                if obj['LastModified'] < thirty_days_ago:
+                    objects_to_delete.append({'Key': obj['Key']})
+    
+    # Delete the objects that are older than 30 days
+    if objects_to_delete:
+        response = s3.delete_objects(
+            Bucket=bucket,
+            Delete={
+                'Objects': objects_to_delete
+            }
+        )
+        print(f"Deleted {len(objects_to_delete)} objects")
+    else:
+        print("No objects older than 30 days to delete")
+
+
 # Run the function
 list_old_files(bucket_name, folder_path)
+
+# Run the function
+delete_old_files(bucket_name, folder_path)
